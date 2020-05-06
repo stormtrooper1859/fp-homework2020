dc326286a224fdc9336c7f7f28e7def02f98abb5
Aleksandr Osipov <sashaosipov.98@gmail.com>
2020-05-06 15:16:32 +0300

    something before optparse

--- hw2/app/Main.hs
+++ hw2/app/Main.hs
@@ -1,6 +1,72 @@
-module Main where
+{-# LANGUAGE OverloadedStrings #-}
 
-import Lib
+module Main where
+-- import Control.Monad
+-- import Control.Monad.Reader
+import Control.Monad.Trans
+import Control.Monad.Trans.Reader
+-- import Control.Monad.Trans.Maybe
+-- import Control.Applicative
+import Data.IORef
+import System.Environment
+import System.Directory
+import System.FilePath
+
+-- import Lib
+
+-- программа пишет текущий каталог, изменяет размер переменной, пишет его занчение
+
+data Env = Env { rootPath :: FilePath
+                , currentPath :: IORef String
+                , storedValue :: IORef Int }
+
+commandLoop :: ReaderT Env IO Env
+commandLoop = do
+    env <- ask
+    let root = rootPath env
+    relPath <- lift $ readIORef $ currentPath env
+    lift $ putStr $ (normalise (root </> relPath)) ++ " > "
+    t <- lift getLine
+    if
+        t == "q"
+    then do
+        t2 <- ask
+        return t2
+    else
+        commandLoop
+
+
+
+getRoot :: IO String
+getRoot = return "./hw1/test"
+
+
+-- data Metainfo = Metainfo { editDate :: String }
+
+data FileSystem = Directory { directoryName :: String, getChildrens :: [FileSystem] } | File { fileName :: String } deriving (Show)
+
+getFileSystem :: FilePath -> IO FileSystem
+getFileSystem path = do
+    isDirectory <- doesDirectoryExist path
+    if not isDirectory then do
+        return $ File $ takeFileName path
+    else do
+        childrens <- listDirectory path
+        rez <- mapM (getFileSystem . (path </>)) childrens
+        return $ Directory path rez
 
 main :: IO ()
-main = someFunc
+main = do
+    args <- getArgs
+    -- putStrLn $ concat args
+    root <- getRoot
+    currentDirectory <- getCurrentDirectory
+    let realRoot = currentDirectory </> root
+    fs <- getFileSystem $ realRoot
+    putStrLn $ show fs
+    refRoot <- newIORef "."
+    zero <- newIORef 0
+    t <- runReaderT (commandLoop) (Env realRoot refRoot zero)
+    rez <- readIORef $ currentPath t
+    return ()
+    -- putStrLn $ rez
--- hw2/src/Lib.hs
+++ hw2/src/Lib.hs
@@ -1,6 +1,46 @@
 module Lib
     ( someFunc
+    , main1
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Debug.Trace
+import Control.Monad
+import Control.Monad.Trans
+import Control.Monad.Trans.Maybe
+import Control.Applicative
+
+-- someFunc :: IO ()
+-- someFunc = putStrLn "someFunc"
+
+
+someFunc :: Int -> Int
+someFunc 0 = 1
+someFunc n = let x = someFunc (n - 1) in traceShow x $ n * x
+
+
+
+emailIsValid :: String -> Bool
+emailIsValid email = '@' `elem` email
+
+askEmail :: MaybeT IO String
+askEmail = do
+    lift $ putStrLn "Input your email, please:"
+    email <- lift getLine
+    guard $ emailIsValid email
+    return email
+
+main1 :: IO ()
+main1 = do
+    Just email <- runMaybeT $ untilSuccess askEmail
+    putStrLn $ "OK, your email is " ++ email
+
+untilSuccess :: Alternative f => f a -> f a
+untilSuccess = foldr (<|>) empty . repeat
+
+
+
+-- doesPathExist :: FilePath -> Reader Env
+
+-- changeDirectoryOnSuccessful :: FilePath -> FilePath -> Either String FilePath
+-- changeDirectoryOnSuccessful a b = let x = a </> b in if doesPathExist x
+
